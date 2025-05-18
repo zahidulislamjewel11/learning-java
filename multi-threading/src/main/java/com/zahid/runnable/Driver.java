939b09af0a07ccd15a8298aaeeb2ae5a359dff88
zahidulislamjewel11 <zahidulislamssf@gmail.com>
2025-05-18 13:38:40 +0600

    revision update

--- multi-threading/src/main/java/com/zahid/runnable/Driver.java
+++ multi-threading/src/main/java/com/zahid/runnable/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		ABC obj1 = new ABC();
-		DEF obj2= new DEF();
+		DEF obj2 = new DEF();
 		
 		Thread s = new Thread(obj1);
 		Thread t = new Thread(obj2);
